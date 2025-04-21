a68d42515ef140593b3c8b61712a3bde8463e48b
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-21 19:05:57 +0530

    basic

---
+++ Chapter-2/02_datatypes.py
@@ -0,0 +1,9 @@
+a = 1 #a is integer
+
+b = 5.22 #b is floting number
+
+c = "laksh" #c is a string
+
+d = True #d is boolean type variable
+
+e = None #e is none type variable
---
+++ Chapter-2/03_type.py
@@ -0,0 +1,6 @@
+a = "13.5"
+d = float(a)
+e = type(d)
+b = type(a)
+print(b)
+print(e)

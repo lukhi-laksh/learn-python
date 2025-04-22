7094d711749a05555e673085c76791a3e58039aa
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-22 13:34:54 +0530

    Variables, DataType, UserInput and Operators

---
+++ Chapter-2/05_Operator.py
@@ -0,0 +1,70 @@
+#Arithemetic Operator
+
+a = 34
+
+b = 4
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+# Assignment Operator
+
+g = 3-2 # Asign 3-2 in g
+print(g)
+h = 4
+h += 3 # Increment the value of b by 3 and then assigin it in b
+h -= 3 # Decrement the value of b by 3 and then assigin it in b
+print(h)
+
+
+# Comperision Operator
+
+i = 5 < 3
+j = 5 > 3
+k = 5 <= 3
+l = 5 >= 3
+m = (5!=3)
+n = 5==5
+
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
+
+# Logican Operator
+
+# or Operator
+o = True or False
+p = False or True
+q = True or True
+r = False or False
+
+print("True or False is:", o)
+print("False or True is:", p)
+print("True or True is:", q)
+print("False or False is:", r)
+
+
+# and Operator
+
+s = True and False
+t = False and True
+u = True and True
+v = False and False
+
+print("True and False is:", s)
+print("False and True is:", t)
+print("True and True is:", u)
+print("False and False is:", v)
+
+print(not(True))
+print(not(False)) 
---
+++ Chapter-2/PS/Problem-4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your Number: "))
+
+print("The squre of number is: ", a**2)
+print("The squre of number is: ", a*a)
+
+
+print("The squre of number is: ", a^a) # Not Valid

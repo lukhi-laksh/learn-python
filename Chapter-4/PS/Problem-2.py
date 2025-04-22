3aad1f61e42543b22a671411095850b05e964149
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-22 15:20:53 +0530

    problem solved!

---
+++ Chapter-4/PS/Problem-2.py
@@ -0,0 +1,19 @@
+student_marks = []
+
+m1 = input("Enter marks of Student 1: ")
+student_marks.append(m1)
+m2 = input("Enter marks of Student 2: ")
+student_marks.append(m2)
+m3 = input("Enter marks of Student 3: ")
+student_marks.append(m3)
+m4 = input("Enter marks of Student 4: ")
+student_marks.append(m4)
+m5 = input("Enter marks of Student 5: ")
+student_marks.append(m5)
+m6 = input("Enter marks of Student 6: ")
+student_marks.append(m6)
+
+print(student_marks)  
+
+student_marks.sort()
+print(student_marks)

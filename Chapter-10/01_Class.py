8035942fe6f0fee0dbe44771e94bb029397f63e8
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-28 14:06:12 +0530

    class and objectg

---
+++ Chapter-10/01_Class.py
@@ -0,0 +1,14 @@
+class Student:
+    name = "laksh"
+
+s1 = Student()
+print(s1)
+
+class Car():
+    color = "green"
+    brand = "Swift"
+
+s2 = Car()
+print(s2.color)
+print(s1.name)
+print(s2.brand)
---
+++ Chapter-10/02_Mehod.py
@@ -0,0 +1,11 @@
+class Student():
+    
+    def __init__(self, name, age):
+        print("this is print autometicly")
+        self.name = name
+        self.age = age
+        print(name, age)
+
+s1 = Student("harsh", 44)
+
+

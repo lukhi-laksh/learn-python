ec34c5b3b956b5a1db7206d499eabc59f27cf507
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-29 17:28:56 +0530

    problem solved

---
+++ Chapter-12/PS/Problem-3.py
@@ -0,0 +1,13 @@
+#this program is use for password managager
+class Account():
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass
+
+    def reset_pass(self):
+        print(self.__acc_pass)
+
+acc1 = Account(12345, "Passwords")
+
+print(acc1.acc_no)
+acc1.reset_pass()

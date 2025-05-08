36468026855b5086b559e964fb6bef026e99810c
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-08 17:36:58 +0530

    remove comment

--- Chapter-17/02_Duplicate.py
+++ Chapter-17/02_Duplicate.py
@@ -1,12 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums):
-        if not nums:  # Handle empty list case
+        if not nums:  
             return 0
 
-        j = 0  # Pointer for the position of unique elements
+        j = 0 
         for i in range(1, len(nums)):
             if nums[j] != nums[i]:
-                j += 1  # Move to the next position for unique element
-                nums[j] = nums[i]  # Assign the unique element
+                j += 1 
+                nums[j] = nums[i]
 
-        return j + 1  # Return the length of the list with unique elements
+        return j + 1 

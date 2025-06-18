fa2b1f006bc970414071a02a77fe3054080a1636
lukhi-laksh <luxlukhi04@gmail.com>
2025-06-18 16:49:37 +0530

    work in progress

--- Chapter-19/DSA Concepts/Stack/MaximalRactangle.py
+++ Chapter-19/DSA Concepts/Stack/MaximalRactangle.py
@@ -1,10 +1,14 @@
 class Solution:
     def LeftMinPrivious(self, Sarr):
-        
+        empty = [0] * len(Sarr[0])
         for i in range(len(Sarr)):
             for j in range(len(Sarr[0])):
-                 if j == 0:
-                     
+                if Sarr[i][j] == 0:
+                    empty[j] = 0
+                else:
+                    empty[j] += 1
+            
+
                 
 
 

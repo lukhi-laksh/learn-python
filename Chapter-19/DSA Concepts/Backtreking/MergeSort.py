a2ac61436aadb7446293058536ddacbd7bcb7e15
lukhi-laksh <luxlukhi04@gmail.com>
2025-07-08 16:53:24 +0530

    work in progres

--- Chapter-19/DSA Concepts/Backtreking/MergeSort.py
+++ Chapter-19/DSA Concepts/Backtreking/MergeSort.py
@@ -1,6 +1,34 @@
 def laksh(arr, start, end):
-    pass
+    def Merge(arr, start, mid, end):
+        temp = [0] * (end - start + 1)
+        left = start
+        right = mid + 1
+        index = 0
+        
+        while left <= mid and right <= end:
+            if arr[left] <= arr[right]:
+                temp[index] = arr[left]
+                left += 1
+            else:
+                temp[index] = arr[right]
+                right += 1
+            index += 1
+        
 
+    
+    def MergeSort(arr, start, end):
+        if start >= end:
+            return
+        
+        mid = start + (end - start) // 2
+        
+        MergeSort(arr, start, mid)
+        MergeSort(arr, mid + 1, end)
+        Merge(arr, start, mid, end)
+    
+        return arr
+
+    return MergeSort(arr, start, end) or arr
 
 arr = [2, 7, 4, 6, 1, 3, 5, 7, 9, 8]
-laksh(arr, 0, len(arr)-1)
+print(laksh(arr, 0, len(arr)-1))

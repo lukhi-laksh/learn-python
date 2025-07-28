258715e061ee2ffdfb5878df77c36793930ed530
lukhi-laksh <luxlukhi04@gmail.com>
2025-07-28 19:26:17 +0530

    sum of 1

--- Chapter-19/DSA Concepts/Backtreking/NBinaryNUmber.py
+++ Chapter-19/DSA Concepts/Backtreking/NBinaryNUmber.py
@@ -3,8 +3,9 @@ def binN(s, i, j, ans):
         ans.append(s)
         return
 
-    binN(s + "0", i + 1, j, ans)
-
     if j < i:
         binN(s + "1", i, j + 1, ans)
+    
+    
+    binN(s + "0", i + 1, j, ans)
 
